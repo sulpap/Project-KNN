e7af0383cf9c1f56659f9e3298bdbf04360a3442
GeorgeKorillos <george.korillos@gmail.com>
2024-10-11 17:58:31 +0300

    starting class

--- include/header.hpp
+++ include/header.hpp
@@ -1,7 +1,42 @@
 #ifndef HEADER_HPP
 #define HEADER_HPP
 
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+#include <set>
 
+using namespace std;
 
+class Point {
+    private:
+        int id;
+        vector<double> syntetagmenes;
+        list<Point> edges;
+
+    public:
+        Point(int id, vector<double> syntetagmenes, list<Point> edges) {
+            this->id = id;
+            this->syntetagmenes = syntetagmenes;
+            this->edges = edges;
+        } // constructor
+
+        int get_id() {
+            return id;
+        }
+};
+
+class Graph {
+    private:
+        map<int, Point> adj_list;
+
+    public:
+        void insert(Point a) {
+            adj_list.insert(a.get_id(), a);
+            adj_list[a.get_id()]
+        }
+
+};
 
 #endif
