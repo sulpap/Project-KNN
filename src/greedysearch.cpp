dfc37a033840779621ddbe7258039f528586fceb
sulpap <sdi2000162@di.uoa.gr>
2024-10-11 13:00:47 +0300

    greedy

--- src/greedysearch.cpp
+++ src/greedysearch.cpp
@@ -1,4 +1,31 @@
 #include <iostream>
 #include "../include/header.hpp"
+#include <vector>
+#include <set>
 
 using namespace std;
+
+class Point {
+    public:
+        vector<double> syntetagmenes;
+        Point(vector<double> Syntetagmenes) : syntetagmenes(Syntetagmenes) {} // constructor
+}
+
+// start point
+// se kathe vhma koitaw geitones
+// ypologizw apostash apo query
+// epilegw closest geitona
+// ksana
+
+vector<Point> greedy_search(const Point start_node, const Point query, int k, int sizeL)
+{
+    // initialize L and V
+    set<Point> L, V;
+    L.insert(start_node);
+
+
+
+    vector<Point> result;
+    //ypologismos me while < k
+    return result;
+}
