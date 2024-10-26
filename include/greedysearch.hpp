02df32f0e529f6da4c139ad07f37332b7d570007
VassTs <vasstsan@yahoo.gr>
2024-10-26 17:41:18 +0300

    greedy v2

--- include/greedysearch.hpp
+++ include/greedysearch.hpp
@@ -3,9 +3,6 @@
 
 using namespace std;
 
-// vector<int> GreedySearch(Graph &graph, int startNode, vector<double> &queryCoords, int k);       // Θεοδωσία's
-
-
 // L_set, V_set should be empty when calling the function
 // L_set, V_set will be full once function call terminated
-void GreedySearch(Graph &graph, int startNodeId, vector<double> &queryCoords, int k, int L, set<int> &L_set, set<int> &V_set); 
+void GreedySearch(Graph &graph, Node* start_node, vector<double> &queryCoords, int k, int L, set<Node*> &L_set, set<Node*> &V_set);
