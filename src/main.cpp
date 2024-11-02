5c7f301738a60335a3b32693fc2e50d5295ba392
VassTs <61576674+VassTs@users.noreply.github.com>
2024-11-02 16:49:22 +0200

    main v1 (#5)

--- src/main.cpp
+++ src/main.cpp
@@ -1,122 +1,118 @@
-#include <iostream>
-#include <cmath> 
-#include <fstream>
 #include "../include/graph.hpp"
 #include "../include/utility.hpp"
 #include "../include/fvecs_read.hpp"
-#include "../include/generate_graph.hpp"
+#include "../include/ivecs_read.hpp"
 #include "../include/vamana.hpp"
-
-#include <chrono>   // temp --- for time in findMedoid
-
+#include "../include/greedysearch.hpp"
+#include <cassert>
+#include <algorithm>        // due to use of find()
+#include <iostream>
 using namespace std;
 
-
-int main(int argc, char* argv[]) 
-{
-    //TODO query,ground truth
-
-    if (argc != 5) {
-        cout << "Usage: " << argv[0] << " <fvecs base file> <fvecs query file> <ground truth file> <number of nearest neighbors to find (k)>" << endl;
+int main(int argc, char* argv[]) {
+    if (argc != 8) {
+        cout << "Usage: " << argv[0] << " <base file> <queries file> <ground truth file> <a> <L> <R> <k>" << endl;
         return 1;
     }
 
-    const char* base_filename = argv[1]; //"../datasets/siftsmall/siftsmall_base.fvecs";
-    const char* query_filename = argv[2]; //"../datasets/siftsmall/siftsmall_query.fvecs";
-    const char* groudtruth_filename = argv[3]; //"../datasets/siftsmall/siftsmall_groundtruth.fvecs";
-    int k = stoi(argv[4]);
-
-    // ----------------------- Read the base fvecs file given -------------------------
-
-    // Read all vectors from the fvecs file
-    vector<vector<float>> vectors = fvecs_read(base_filename);
-
-    // verify the number of vectors read
-    if (!vectors.empty()) {
-        cout << "Total vectors read: " << vectors.size() << endl;
+// 1. Διάβασμα base file
+    const char* base_filename = argv[1];
+    vector<vector<float>> base_f = fvecs_read(base_filename);
+    
+    // Check size of base_f
+    if (!base_f.empty()) {
+        cout << "Number of points (base.fvecs): " << base_f.size() << endl;
     } else {
-        cout << "No vectors read from the file." << endl;
+        cout << "No base vectors read from the file." << endl;
         return EXIT_FAILURE;
     }
 
-    // float to double to use for nodes
-    vector<vector<double>> coordinates;
-    for (const auto& vec : vectors) {
-        coordinates.push_back(vector<double>(vec.begin(), vec.end()));
-    }
+    vector<vector<double>> base = convert_to_double(base_f); 
 
-    cout << "Base vectors processed successfully." << endl;
-
-    // ----------------------- Read the query fvecs file given -------------------------
-
-    // same process
-
-    vector<vector<float>> temp_queries = fvecs_read(query_filename);
-
-    // verify the number of vectors read
-    if (!temp_queries.empty()) {
-        cout << "Total queries read: " << temp_queries.size() << endl;
-    } else {
-        cout << "No queries read from the file." << endl;
-        return EXIT_FAILURE;
-    }
-
-    // float to double to use for nodes
-    vector<vector<double>> queries;
-    for (const auto& query : temp_queries) {
-        queries.push_back(vector<double>(query.begin(), query.end()));
-    }
-
-    cout << "Query vectors processed successfully." << endl;
-
-    // ----------------------------- findMedoid ------------------------------------
-
-    // auto start = chrono::high_resolution_clock::now();
-
-    // int imedoid = findMedoid(coordinates);
-
-    // auto end = chrono::high_resolution_clock::now();
-    // chrono::duration<double> duration = end - start;
-    // cout << "findMedoid took: " << duration.count() << " seconds" << endl;
-
-    // cout << "medoid index is:" << imedoid << endl;
-
-    // cout << "medoid coordinates: ";
-    // for (const auto& value : coordinates[imedoid]) {
-    //     cout << value << " ";
-    // }
-    // cout << endl;
-
-   // ------------------------------- vamana ----------------------------------
+// 2. Κλήση Vamana
+    double a = stod(argv[4]); 
+    cout << "a = " << a << endl;
+    int l = stoi(argv[5]);
+    cout << "l = " << l << endl;
+    int r = stoi(argv[6]);
+    cout << "r = " << r << endl;
 
     Graph graph;
+    cout << "Start of Vamana call from main" << endl;
+    int medoid_id = Vamana(graph, base, r, a, l);
+    cout << "End of Vamana call from main" << endl;
 
-    // vector<vector<double>> coords = {
-    //     {1.0, 2.0},
-    //     {2.0, 3.0},
-    //     {3.0, 4.0},
-    //     {4.0, 5.0},
-    //     {2.0, 1.0}
-    // };
+    Node* medoid = graph.getNode(medoid_id);
 
-    int R = 3;
-    // k = 2;
-    int a = 1; // a should be float -> 1.2
-    int int_L = 10000;
-
-    cout << "running vamana..." << endl;
-    int med = Vamana(graph, coordinates, R, a, int_L);
+// 3. Διάβασμα queries file
+    const char* query_filename = argv[2];
+    vector<vector<float>> query_f = fvecs_read(query_filename);
     
-    // cout << endl << "Cleaning..." << endl;
+    // Check size of query_f
+    if (!query_f.empty()) {
+        cout << "Number of queries (query.fvecs): " << query_f.size() << endl;
+    } else {
+        cout << "No queries vectors read from the file." << endl;
+        return EXIT_FAILURE;
+    }
 
-    // delete node1;
-    // delete node2;
-    // delete node3;
-    // delete node4;
-    // delete node5;
-    // delete node6;
+    vector<vector<double>> queries = convert_to_double(query_f); 
 
-    cout << endl << "Bye!" << endl;
+// 4. Διάβασμα ground truth για κάθε query
+    const char* groudtruth_filename = argv[3];
+    vector<vector<int>> ground_truth = ivecs_read(groudtruth_filename);
+
+    // Check size of ground_truth
+    if (!ground_truth.empty()) {
+        cout << "Number of ground_truth (ground_truth.ivecs): " << ground_truth.size() << endl;
+    } else {
+        cout << "No ground_truth vectors read from the file." << endl;
+        return EXIT_FAILURE;
+    }
+
+
+// 5. Κλήση greedy για κάθε query
+    int k = stoi(argv[7]);
+    cout << "k = " << k << endl;
+
+    for(int i = 0; i < static_cast<int>(queries.size()); i++) {
+        vector<double> query = queries[i];
+        set<Node*> L_set;
+        set<Node*> V_set;
+
+        cout << "Greedy call for " << i << "th query" << endl;
+        GreedySearch(medoid, query, k, l, L_set, V_set);
+
+// 6. Σύγκριση greedy με ground truth
+        vector<int> gt_sol = ground_truth[i];
+        if(k > static_cast<int>(gt_sol.size())) {
+            cout << "For query (zero based) #"<< i << " can't compare greedy vs ground truth. Could compare with k up to " << gt_sol.size() << endl;
+            return EXIT_FAILURE;
+        }
+        vector<int> gt(gt_sol.begin(), gt_sol.begin() + k);     // Μάς ενδιαφέρουν για σύγκριση μόνο τα top k αποτελέσματα του ground truth
+
+        // We traverse L_set
+        int found = 0;
+        for (auto node : L_set) {
+            int id_L = node->getId();
+            if(find(gt.begin(), gt.end(), id_L) != gt.end()) {
+                found++;
+            }
+        }
+        float percent = (100 * found) / (float)k;
+        cout << "Query (zero based) #" << i << " had " << percent << "% success running greedy after vamana" << endl;
+    }
+
+    graph.clear();      // once done
+
+    cout << "Bye from main" << endl;
 
     return 0;
 }
+
+/*
+[0] Example of execution:
+./bin/main datasets/siftsmall/siftsmall_base.fvecs datasets/siftsmall/siftsmall_query.fvecs datasets/siftsmall/siftsmall_groundtruth.ivecs 1.2 10000 12 1
+
+[1] Μέχρι να απαντήσει ο Πασκαλής στο @40 θεωρούμε ότι το a δίνεται από τη γραμμή εντολών
+*/
