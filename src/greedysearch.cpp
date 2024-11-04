2a2bc4df11214f7eb595a1bc2d3ec3447ee869e2
sulpap <sdi2000162@di.uoa.gr>
2024-11-04 18:34:13 +0200

    removed useless comments

--- src/greedysearch.cpp
+++ src/greedysearch.cpp
@@ -27,7 +27,7 @@ void GreedySearch(Node* start_node, vector<double> &queryCoords, int k, int L, s
     L_set.insert(start_node);  // Initialization of L_set
     // V_set is empty
 
-    set<Node*> LminusV;               // initilize difference set to store difference of L\V
+    set<Node*> LminusV;            // initilize difference set to store difference of L\V
     LminusV.insert(start_node);    // L\V = {start} \ {} = {start}
 
     while(LminusV.empty() == false) {       // while LminusV != {}        
@@ -47,14 +47,14 @@ void GreedySearch(Node* start_node, vector<double> &queryCoords, int k, int L, s
         Node* p_star = min_node;
 
         // Update V_set [V = V U p*]
-        V_set.insert(p_star);       //  if p_star is already in V_set, p_set won't be inserted in V
+        V_set.insert(p_star);       // if p_star is already in V_set, p_set won't be inserted in V
         
         // Update L_set [ L = L U Nout(p*) ]
-        list<Node*> p_star_out = p_star->getEdges();            // Οι εξερχόμενοι γείτονες του p*
+        list<Node*> p_star_out = p_star->getEdges();            // out-neighbors of p*
         L_set.insert(p_star_out.begin(), p_star_out.end());
 
         // We cast L to long unsigned int, since that's what set.size returns
-        if(static_cast<int>(L_set.size()) > L) {                  // Update L to retain closests L points to query
+        if(static_cast<int>(L_set.size()) > L) {                // Update L to retain closests L points to query
             vector<struct info> vector_info;
             // We traverse L and save it to vector
             for (auto node : L_set) {
@@ -108,67 +108,3 @@ void GreedySearch(Node* start_node, vector<double> &queryCoords, int k, int L, s
 
     return;
 }
-
-
-
-
-// #include <set>
-// #include <queue>
-// #include <cmath>
-// #include <algorithm>
-// #include "../include/graph.hpp"
-// #include "../include/utility.hpp"
-
-// // GreedySearch function
-// void GreedySearch(Graph& G, Node* s, const vector<double>& x_q, int k, int L, set<Node*> &back_L_set, set<Node*> &V_set) {
-//     // Initialize the result set L_set and the visited set V
-//     set<Node*> V;
-//     vector<Node*> L_set = { s };
-//     V.insert(s);
-
-//     while (!L_set.empty()) {
-//         // Step 1: Find the closest node p* in L_set \ V to x_q
-//         Node* pStar = nullptr;
-//         double minDistance = numeric_limits<double>::max();
-//         for (Node* node : L_set) {
-//             if (V.find(node) == V.end()) { // Only consider nodes not in V
-//                 double dist = euclidean_distance(node->getCoordinates(), x_q);
-//                 if (dist < minDistance) {
-//                     minDistance = dist;
-//                     pStar = node;
-//                 }
-//             }
-//         }
-
-//         // Step 2: Stop if no unvisited node remains
-//         if (pStar == nullptr) break;
-
-//         // Step 3: Update L_set and V
-//         for (Node* neighbor : pStar->getEdges()) {
-//             if (find(L_set.begin(), L_set.end(), neighbor) == L_set.end()) {
-//                 L_set.push_back(neighbor);
-//             }
-//         }
-//         V.insert(pStar);
-
-//         // Step 4: Prune L_set if it exceeds L by retaining the closest L points to x_q
-//         if (L_set.size() > static_cast<size_t>(L)) {
-//             sort(L_set.begin(), L_set.end(), [&](Node* a, Node* b) {
-//                 return euclidean_distance(a->getCoordinates(), x_q) < euclidean_distance(b->getCoordinates(), x_q);
-//             });
-//             L_set.resize(L);
-//         }
-//     }
-
-//     // Sort and return the closest k points from L_set and the visited nodes V
-//     sort(L_set.begin(), L_set.end(), [&](Node* a, Node* b) {
-//         return euclidean_distance(a->getCoordinates(), x_q) < euclidean_distance(b->getCoordinates(), x_q);
-//     });
-//     if (L_set.size() > static_cast<size_t>(k)) {
-//         L_set.resize(k);
-//     }
-
-//     for (Node* l : L_set) {
-//         back_L_set.insert(l);
-//     }
-// }
