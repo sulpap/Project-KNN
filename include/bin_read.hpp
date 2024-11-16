437b6a9ee3dfcc760c287d2d42a14b988be1902a
VassTs <61576674+VassTs@users.noreply.github.com>
2024-11-16 20:45:43 +0200

    added: functions to read points, queries (#14)

---
+++ include/bin_read.hpp
@@ -0,0 +1,19 @@
+#ifndef BIN_READ_HPP
+#define BIN_READ_HPP
+
+#include <vector>
+using  namespace std;
+
+/* Συνάρτηση που διαβάζει ένα data.bin αρχείο και επιστρέφει
+vector <vector<float>> points, όπου το κάθε vector στο 0-στο στοιχείο έχει
+την τιμή του C (>=0), ενώ από το 1ο στοιχείο μέχρι και το 101 στοιχείο είναι
+οι συντεταγμένες του σημείου */
+vector<vector<float>> databin_read(const char* filename);
+
+/* Συνάρτηση που διαβάζει ένα queries.bin αρχείο και επιστρέφει vector <vector<float>> queries,
+όπου το κάθε vector στο 0-στο στοιχείο έχει τιμή 0 ή 1 (query type),
+στο 1ο στοιχείο έχει τιμή του C (>=0 αν έχει φίλτρο ή -1 αν δεν έχει φίλτρο),
+ενώ από το 2ο στοιχείο μέχρι και το 102 στοιχείο είναι οι συντεταγμένες του query */
+vector<vector<float>> queriesbin_read(const char* filename);
+
+#endif
