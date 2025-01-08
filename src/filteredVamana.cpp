822b3a4fdae7c1f0a1afcfa32794938f5d11996e
VassTs <61576674+VassTs@users.noreply.github.com>
2025-01-08 22:06:36 +0200

    filtered: save distances in vamana in parallel (#62)

--- src/filteredVamana.cpp
+++ src/filteredVamana.cpp
@@ -14,6 +14,7 @@
 #include "../include/filteredrobustprune.hpp"
 #include "../include/FindMedoid.hpp"
 #include "../include/generate_graph.hpp"
+#include "../include/utility.hpp"               // due to use of euclidean_distance_of_nodes
 #include <algorithm>
 #include <iostream>
 #include <random>
@@ -134,6 +135,78 @@ void *process_chunk(void *args)
     return nullptr;
 }
 
+
+#define THREADS_NO 10
+
+// Παράμετροι που θα λάβει ένα thread για τον υπολογισμό των αποστάσεων
+struct arguments {
+    map<int, Node*> *nodes;
+    map<int, Node*>::iterator it_begin;
+    map<int, Node*>::iterator it_end;
+    unordered_map<pair<Node*, Node*>, double, PairHash> *nodePairMap;
+};
+
+// Συνάρτηση που υπολογίζει τις παραμέτρους που θα λάβει κάθε thread που τρέχει την συνάρτηση childThread
+static void fill_up_args(map<int, Node*> &nodes, vector<unordered_map<pair<Node*, Node*>, double, PairHash>> &maps, int threads_no, struct arguments *array) {
+
+    int size = nodes.size();
+    int chunk = size / threads_no;  // (πόσα nodes έχει κάθε thread)
+    if(size % threads_no != 0)
+        chunk++;
+
+    int has_done = 0;
+    for(int i = 0; i < threads_no; i++) {
+        int offset = chunk;     // 4
+        if(has_done + offset > size)
+            offset = size - has_done;
+
+
+        if(i == 0)
+            array[i].it_begin = nodes.begin();
+        else {
+            array[i].it_begin = array[i-1].it_end;
+        }
+
+        array[i].it_end = array[i].it_begin;
+        advance(array[i].it_end, offset);
+        has_done += offset;
+
+        array[i].nodes = &nodes;
+        array[i].nodePairMap = &(maps[i]);
+    }
+    return;
+}
+
+// Συνάρτηση που τρέχει ένα thread που υπολογίζει τις αποστάσεις. Λαμβάνει ως παράμετρο ένα struct arguments*
+void* childThread (void* args) {
+    struct arguments *args_c = (struct arguments*)args;       // args_child
+
+    for (auto it = args_c->it_begin; it != args_c->it_end; ++it) {           // [0. size_div_2)
+        Node* node = it->second;
+        int label = node->getLabel();
+
+        for (auto it2 = it; it2 != args_c->nodes->end(); ++it2) {      // we start from it2 = it instead of it2 = it + 1, because we want to store that the distance of one node from itself is zero
+            Node* node2 = it2->second;
+            int label2 = node2->getLabel();
+
+            // Skip if nodes don't have the same label
+            if (label != label2)
+                continue;
+
+            // Create an order-independent key
+            pair<Node*, Node*> key = {min(node, node2), max(node, node2)};      // The hash and key use min and max to ensure that {node1, node2} is treated the same as {node2, node1}. This eliminates the need to insert the symmetric pair manually.
+            double dist = euclidean_distance_of_nodes(node, node2);
+
+            // Insert if the key does not exist
+            assert(args_c->nodePairMap->find(key) == args_c->nodePairMap->end());     // shouldn't exist already        
+            (*args_c->nodePairMap)[key] = dist; // Insert the key-value pair
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
+
 Graph filteredVamana(vector<vector<double>> &coords, double a, int int_L, int R, set<int> F, int taph, map<int, Node *> &st_f) 
 {
     Graph G;
@@ -142,6 +215,33 @@ Graph filteredVamana(vector<vector<double>> &coords, double a, int int_L, int R,
     // generate_random_edges(G, 2);
 
     st_f = FindMedoid(G, taph, F);
+
+// Αποθήκευση αποστάσεων
+    // Vector to hold THREADS_NO unordered_map objects
+    vector<unordered_map<pair<Node*, Node*>, double, PairHash>> maps(THREADS_NO);        // we calculate & store only the distances between nodes of the same label 
+    map<int, Node*> nodes = G.getAdjList();
+
+    pthread_t thread_id[THREADS_NO];        // Store the threadIDs in an array
+    struct arguments array[THREADS_NO];     // Store the struct arguments of each thread in an array
+    fill_up_args(nodes, maps, THREADS_NO, array);            // Prepare arguments for each thread
+
+    // Create a new thread. The new thread will run the childThread function.
+    for (int threadCount = 0; threadCount < THREADS_NO; threadCount++){
+        pthread_create (&thread_id[threadCount], NULL, &childThread, &(array[threadCount]));
+    }
+
+	for (int threadCount = 0; threadCount< THREADS_NO; threadCount++) {    // Join after all thread creation
+    	void *status;
+		pthread_join(thread_id[threadCount], &status);
+    }	
+
+    // Combine all maps into one
+    unordered_map<pair<Node*, Node*>, double, PairHash> nodePairMap;
+    for (const auto& map : maps) {
+        nodePairMap.insert(map.begin(), map.end());
+    }
+    // Size of nodePairMap should be 11029349 (without the zero distances added)
+// Τέλος αποθήκευσης αποστάσεων
 
     vector<int> randomPermutation(coords.size());
     iota(randomPermutation.begin(), randomPermutation.end(), 0);            // starts from 0 until coords.size - 1
